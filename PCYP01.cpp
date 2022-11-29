6d832bec2ca1b8c2852ababc885d70ca3a933ead
ariadna-ge <Aridenn1503@hotmail.com>
2022-11-29 17:18:48 -0600

    New version that reads a file, but now converts a word to a lowercase

--- PCYP01.cpp
+++ PCYP01.cpp
@@ -1,22 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-int main()
-{
-    //lectura de archivo
+void Diccionario(){
+	//lectura de archivo
     FILE* archive;
-    char c;
+	char word;
 
     //abrir archivo
     fopen_s(&archive, ".txt", "w");
     if (archive != NULL){
-        scanf_s("%[^\n]", &c);
-        fflush(stdin);
-        //limpieza buffer
-    }
+    	scanf("%[^\n]", &word);
+    	fflush(stdin);
+		//remover caracteres
+		
+      //escribir en minuscula
+        for(in i=0, word[i]!='\0'; i++){
+        	word[i] = tolower(word[i]);
+		}
+		printf("%s", word);	
+		
+		}
     else{
         printf("\n No detecta archivo \n\n");
         }
     fclose(archive);
+}
+
+int main()
+{
     return 0;
 }
