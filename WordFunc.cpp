5d939091858ef39a300a634d881e01049bc25140
ariadna-ge <Aridenn1503@hotmail.com>
2022-11-30 19:29:46 -0600

    Update of Word Function

---
+++ WordFunc.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	char *szPalabraLeida;
+	char szPalabrasSugeridas[][TAMTOKEN];
+	int iNumSugeridas; 
+	char alfa[] = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+	int con, con1, con2;
+	char wordSp[1000];
+	int wordTemp[1000], lim;
+	
+	lim=0;
+	
+	while (word[lim] != '\0'){
+		strcpy_s(wordTemp, word);
+		con= lim;
+		for(wordTemp[con] != '\0'; wordTemp[con] = worTemp[con+1]; cont++); 
+	}
+}
